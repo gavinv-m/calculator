985e25ad5a56f89da0c02915da2755190eec1e4e
Gavin <gavinvm@protonmail.com>
2023-11-30 23:14:32 +0200

    Store input for both operands

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-const operands = document.getElementsByClassName('operands');
 const display = document.getElementById('calculator-display');
+const equalsButton = document.getElementById('equals-button');
+const operands = document.getElementsByClassName('operands');
+const operators = document.getElementsByClassName('all-operators');
 
 let operandOne = '';
 let operandTwo = '';
+let operator = '';
+let operatorCounter = 0;
+
+let operandOneActive = true;
+let operandTwoActive = false;
+
 
 for (let operand of operands) {
-    // add the event listener to each element
     operand.addEventListener('click', displayText);
   }
+
+for (let operator of operators) {
+  operator.addEventListener('click', (event) => {
+
+    if (operandOneActive) {
+      operandOneActive = false;
+      operandTwoActive = true;
+    }
+
+    operator = event.target.textContent;
+  });
+}
   
 function displayText(event) {
 
-    let text = event.target.textContent;
-    operandOne += text; 
-    display.innerHTML = operandOne;
+  let text; 
+
+    if (operandOneActive) {
+      display.innerHTML = '';
+      text = event.target.textContent;
+      operandOne += text;
+      display.innerHTML = operandOne;
+    }
+
+    else {
+      display.innerHTML = '';
+      text = event.target.textContent;
+      operandTwo += text;
+      display.innerHTML = operandTwo;
+    }
 }
